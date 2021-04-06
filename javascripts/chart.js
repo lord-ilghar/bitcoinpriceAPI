4dcc78ed649cb91913e28baed17919d80f141853
lordilghar <aylqarm@gmail.com>
2021-04-06 20:48:28 +0430

    formatted and added README

--- javascripts/chart.js
+++ javascripts/chart.js
@@ -4,39 +4,38 @@ let xvalue = [];
 
 historyweek();
 
-async function historyweek() 
-{
-    const res = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
-    let data = await res.json();
-    document.getElementById('lastUP').textContent = data.time.updated;
-    data = JSON.stringify(data.bpi);
-    data = data.replace('{' , '');   
-    data = data.replace('}' , '');
-    splitdata = data.split(',') 
-    splitdata.forEach((item) => {
-        const value = item.split(':');
-        value[0] = value[0].replace("\"" , '');
-        value[0] = value[0].replace("\"" , '');
-        xlabels.push(value[0]);
-        xvalue.push(value[1]);
-    })
-    await chart();
+async function historyweek() {
+  const res = await fetch(
+    'https://api.coindesk.com/v1/bpi/historical/close.json'
+  );
+  let data = await res.json();
+  document.getElementById('lastUP').textContent = data.time.updated;
+  data = JSON.stringify(data.bpi);
+  data = data.replace('{', '');
+  data = data.replace('}', '');
+  splitdata = data.split(',');
+  splitdata.forEach((item) => {
+    const value = item.split(':');
+    value[0] = value[0].replace('"', '');
+    value[0] = value[0].replace('"', '');
+    xlabels.push(value[0]);
+    xvalue.push(value[1]);
+  });
+  await chart();
 }
 
 async function chart() {
-    const mychart = new Chart(
-        fram,
+  const mychart = new Chart(fram, {
+    type: 'line',
+    data: {
+      labels: xlabels,
+      datasets: [
         {
-        type: 'line',
-        data: {
-            labels: xlabels,
-            datasets: [{
-                data: xvalue,
-                label: 'price up' ,
-                borderColor: 'green'
-            }],
-           
-        }
-    }
-    )
+          data: xvalue,
+          label: 'price up',
+          borderColor: 'green',
+        },
+      ],
+    },
+  });
 }
--- javascripts/liveprice.js
+++ javascripts/liveprice.js
@@ -1,8 +1,8 @@
 liveprice();
 async function liveprice() {
-    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-    const data = await res.json();
-    console.log(data)
-    document.getElementById('price').textContent = data.bpi.USD.rate + '$'; 
-    document.getElementById('updateTime').textContent = data.time.updated
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+  console.log(data);
+  document.getElementById('price').textContent = data.bpi.USD.rate + '$';
+  document.getElementById('updateTime').textContent = data.time.updated;
 }
